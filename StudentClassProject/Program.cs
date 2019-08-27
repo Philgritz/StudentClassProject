dd9626d31ab2942e9d4c98057ba18ac55c6b8f04
max-student@outlook.com <max-student@outlook.com>
2019-08-27 16:36:39 -0400

    Add project files.

---
+++ StudentClassProject/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace StudentClassProject {
+    class Program {
+        static void Main(string[] args) {
+
+            var Lidya = new Student();
+            Lidya.Firstname = "Lidya";
+            Lidya.Lastname = "Tsegay";
+            Lidya.SetFinalGrade(99);
+            var fullname = Lidya.GetFullname();
+            Console.WriteLine("Lidya's full name is " + fullname);
+
+            var Justin = new Student();
+            Justin.Firstname = "Justin";
+            Justin.Lastname = "Dudley";
+            Justin.SetFinalGrade(98); 
+            fullname = Justin.GetFullname();
+            Console.WriteLine($"{fullname}");
+
+            var Nick = new Student();
+            Nick.Firstname = "Nick";
+            Nick.Lastname = "Baker";
+            Nick.SetFinalGrade(-100);
+            Console.WriteLine($"{Nick.Firstname} {Nick.Lastname}");
+        }
+    }
+}
---
+++ StudentClassProject/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentClassProject {
+    class Student {
+
+
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int FinalGrade { get; set; }
+
+        public string GetFullname() {
+           return Firstname + " " + Lastname;
+        }
+        public void SetFinalGrade(int finalGrade) {
+            if(finalGrade < 0) {
+                Console.WriteLine("ERROR:  grade must be greater than zero");
+            } else {
+              FinalGrade = finalGrade;
+            }
+        }
+    }
+}
